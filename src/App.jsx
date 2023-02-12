91696f4c75a55b30a146ef971f71f28cf8a27e25
herveyyy <captainhervey@gmail.com>
2023-02-12 16:58:31 +0800

    v0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+import DisplayData from "./component/DisplayData"
+import Title from "./component/Title"
+export default function App() {
+
+  return (
+    <div>
+
+      <div className="">
+         <DisplayData />
+      </div>
+      </div>
+  )
+}
---
+++ src/component/Title.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Title() {
+    return ( 
+    
+<div className="">
+<div className="mr-56 w-20 relative ...">
+<a  href="https://liceo.edu.ph/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <img src="https://upload.wikimedia.org/wikipedia/en/1/18/Ldcu_seal.png"
+         />
+        </a>
+        </div>
+       
+        <div className="absolute inset-x-0 top-8 h-16 text-center font-bold">Liceo De Cagayan University</div>
+        <div className="mx-40 absolute inset-x-0 top-12 h-16 text-center">FJPQ+CM7, Rodolfo N. Pelaez Blvd, Cagayan de Oro, 9000 Misamis Oriental</div>
+        
+            <div className="absolute top-0 right-0 w-20 ...">
+        <a  href="https://www.facebook.com/CITLiceoU/"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img src="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/304780468_459787039496930_3361425458901923694_n.png?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEeLXXkQEr3Tatgnj93_WRM497gTUZbilTj3uBNRluKVBRDFFGz4RlCW60RoooSSVTxzyggZxs8JGRr2TU4QNYt&_nc_ohc=yeZ4SOg65L8AX-KpCH9&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfA5lgILWgR5apgua06hk0gvUDilvLIVT-I8H6t2fQxLWQ&oe=63EDEB88"
+         />
+        </a>
+        </div>
+</div>
+
+
+     );
+}
+
+export default Title;
