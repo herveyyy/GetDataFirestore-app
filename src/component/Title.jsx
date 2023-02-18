6cdd4c56e8b5407a77dbe89324e2ad1d7a4fb4a6
herveyyy <captainhervey@gmail.com>
2023-02-18 18:27:29 +0800

    logocit

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -4,11 +4,11 @@ function Title() {
     return ( 
     
 <div className="">
-<div className="mr-56 w-20 relative top-4 left-10 ...">
+<div className="mr-56 w-20 relative top-4 left-10 ">
 <a  href="https://liceo.edu.ph/"
         target="_blank"
         rel="noopener noreferrer"
-        >
+        className="bg-neutral-800">
         <img src="https://upload.wikimedia.org/wikipedia/en/1/18/Ldcu_seal.png"
          />
         </a>
@@ -21,7 +21,7 @@ function Title() {
         <a  href="https://www.facebook.com/CITLiceoU/"
         target="_blank"
         rel="noopener noreferrer">
-        <img src="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/304780468_459787039496930_3361425458901923694_n.png?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEeLXXkQEr3Tatgnj93_WRM497gTUZbilTj3uBNRluKVBRDFFGz4RlCW60RoooSSVTxzyggZxs8JGRr2TU4QNYt&_nc_ohc=yeZ4SOg65L8AX-KpCH9&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfA5lgILWgR5apgua06hk0gvUDilvLIVT-I8H6t2fQxLWQ&oe=63EDEB88"
+        <img src="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/304780468_459787039496930_3361425458901923694_n.png?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEeLXXkQEr3Tatgnj93_WRM497gTUZbilTj3uBNRluKVBRDFFGz4RlCW60RoooSSVTxzyggZxs8JGRr2TU4QNYt&_nc_ohc=5HGEHGGhmZ8AX-PKe6t&tn=c9zqIoHPowXXqFfB&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfCa7WT8aoNmEqK_VTuIEKcEbMHioMVlM4bj31ePaFGLFg&oe=63F5D488"
          />
         </a>
         </div>
